a2479c84246379ccda1d465f0bb5855d40375ee2
contribuyente <monotributista.contribuyente@proton.me>
2023-08-13 15:32:32 -0300

    optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+    },
+  },
 };
 
 export default config;
