896f35e4066b79e341f43934da3a6ace9aa3dad0
contribuyente <monotributista.contribuyente@proton.me>
2023-12-13 10:07:20 -0300

    fix limit

--- tasks/bid.ts
+++ tasks/bid.ts
@@ -12,7 +12,7 @@ task("bid", "Makes a bid on the current auction")
 
     const auctionHouse = await ethers.getContractAt("AuctionHouse", address);
 
-    if (!amount) {
+    if (!amount && !limit) {
       console.log("No amount passed, using reserve price");
     }
 
@@ -23,62 +23,74 @@ task("bid", "Makes a bid on the current auction")
     const reservePrice = await auctionHouse.reservePrice();
     let bid = amount ? ethers.parseEther(amount) : reservePrice;
 
-    const auction = await auctionHouse.auction();
+    let auction = await auctionHouse.auction();
     const endTime = Number(auction.endTime) * 1000;
     const isFinished = endTime < Date.now();
     if (isFinished) {
-      console.log("Current auction is finished");
+      console.log("Current auction is finished, restarting...");
+      const tx = await auctionHouse.settleCurrentAndCreateNewAuction();
+      console.log("Waiting for transaction...");
+      await tx.wait();
+      console.log("Done!");
+      console.log("Updating auction data...");
+      auction = await auctionHouse.auction();
+      console.log("Done!");
+    }
+
+    const [signer] = await ethers.getSigners();
+    const sender = await signer.getAddress();
+
+    if (auction.bidder === sender && limit) {
+      console.log(`The sender is already winning the auction`);
       return;
-    } else {
-      const minBidIncrementPercentage =
-        await auctionHouse.minBidIncrementPercentage();
-      const minIncrement = (auction.amount * minBidIncrementPercentage) / 100n;
-      const minBid =
-        auction.amount === 0n
-          ? reservePrice
-          : ethers.parseEther(
-              Math.ceil(
-                Number(ethers.formatEther(auction.amount + minIncrement))
-              ).toString()
-            );
-      console.log(`Minumim bid:`, ethers.formatEther(minBid));
-      if (limit) {
-        const battleAmount = ethers.parseEther(limit);
-        if (minBid <= battleAmount) {
-          console.log(`Minimum bid is below bid limit`);
-          bid = minBid;
-        } else {
-          console.log(`Minimum bid is above bid limit`);
-        }
-      }
-      console.log("Bid amount:", ethers.formatEther(bid));
-      if (bid < minBid) {
-        console.log(
-          `Bid is too low, minimum bid is ${ethers.formatEther(minBid)} MANA`
-        );
+    }
+
+    const minBidIncrementPercentage =
+      await auctionHouse.minBidIncrementPercentage();
+    const minIncrement = (auction.amount * minBidIncrementPercentage) / 100n;
+    const minBid =
+      auction.amount === 0n
+        ? reservePrice
+        : ethers.parseEther(
+            Math.ceil(
+              Number(ethers.formatEther(auction.amount + minIncrement))
+            ).toString()
+          );
+    console.log(`Minumim bid:`, ethers.formatEther(minBid));
+    if (limit) {
+      const battleAmount = ethers.parseEther(limit);
+      if (minBid <= battleAmount) {
+        console.log(`Minimum bid is below bid limit`);
+        bid = minBid;
       } else {
-        const manaToken = await ethers.getContractAt("ERC20", token);
-        const [signer] = await ethers.getSigners();
-        const owner = await signer.getAddress();
-        let allowance = await manaToken.allowance(owner, address);
-        console.log(`Allowance:`, ethers.formatEther(allowance));
-        if (allowance > 0n && allowance < bid) {
-          console.log(`Resetting allowance...`);
-          const tx = await manaToken.approve(address, 0n);
-          await tx.wait();
-          console.log(`Done!`);
-          allowance = await manaToken.allowance(owner, address);
-        }
-        if (allowance === 0n) {
-          console.log(`Setting allowance to: ${ethers.formatEther(bid)}...`);
-          const tx = await manaToken.approve(address, bid);
-          await tx.wait();
-          console.log(`Done!`);
-        }
-        console.log(`Making bid of ${ethers.formatEther(bid)}...`);
-        const tx = await auctionHouse.createBid(auction.tokenId, bid);
+        console.log(`Minimum bid is above bid limit`);
+      }
+    }
+    console.log("Bid amount:", ethers.formatEther(bid));
+    if (bid < minBid) {
+      console.log(
+        `Bid is too low, minimum bid is ${ethers.formatEther(minBid)} MANA`
+      );
+    } else {
+      const manaToken = await ethers.getContractAt("ERC20", token);
+      let allowance = await manaToken.allowance(sender, address);
+      console.log(`Allowance:`, ethers.formatEther(allowance));
+      if (allowance > 0n && allowance < bid) {
+        console.log(`Resetting allowance...`);
+        const tx = await manaToken.approve(address, 0n);
+        await tx.wait();
+        console.log(`Done!`);
+        allowance = await manaToken.allowance(sender, address);
+      }
+      if (allowance === 0n) {
+        console.log(`Setting allowance to: ${ethers.formatEther(bid)}...`);
+        const tx = await manaToken.approve(address, bid);
         await tx.wait();
         console.log(`Done!`);
       }
+      console.log(`Making bid of ${ethers.formatEther(bid)}...`);
+      const tx = await auctionHouse.createBid(auction.tokenId, bid);
+      await tx.wait();
+      console.log(`Done!`);
     }
   });
