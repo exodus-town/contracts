1e95cf0b61b7d850082b329265164c27802d9292
contribuyente <monotributista.contribuyente@proton.me>
2023-12-13 10:00:13 -0300

    limit

--- tasks/bid.ts
+++ tasks/bid.ts
@@ -4,7 +4,8 @@ task("bid", "Makes a bid on the current auction")
   .addOptionalParam("address", "The address of the AuctionHouse contract")
   .addOptionalParam("token", "The address of the MANA token")
   .addOptionalParam("amount", "The amount of MANA to bid")
-  .setAction(async ({ address, token, amount }, { ethers, network }) => {
+  .addOptionalParam("limit", "The maximum amount of MANA to bid")
+  .setAction(async ({ address, token, amount, limit }, { ethers, network }) => {
     console.log("AuctionHouse:", address);
     console.log("ManaToken:", token);
     console.log("Network:", network.name);
@@ -15,10 +16,12 @@ task("bid", "Makes a bid on the current auction")
       console.log("No amount passed, using reserve price");
     }
 
-    const reservePrice = await auctionHouse.reservePrice();
-    const bid = amount ? ethers.parseEther(amount) : reservePrice;
+    if (limit) {
+      console.log(`Bid limit: ${limit} MANA`);
+    }
 
-    console.log("Bid amount:", ethers.formatEther(bid));
+    const reservePrice = await auctionHouse.reservePrice();
+    let bid = amount ? ethers.parseEther(amount) : reservePrice;
 
     const auction = await auctionHouse.auction();
     const endTime = Number(auction.endTime) * 1000;
@@ -31,8 +34,24 @@ task("bid", "Makes a bid on the current auction")
         await auctionHouse.minBidIncrementPercentage();
       const minIncrement = (auction.amount * minBidIncrementPercentage) / 100n;
       const minBid =
-        auction.amount === 0n ? reservePrice : auction.amount + minIncrement;
+        auction.amount === 0n
+          ? reservePrice
+          : ethers.parseEther(
+              Math.ceil(
+                Number(ethers.formatEther(auction.amount + minIncrement))
+              ).toString()
+            );
       console.log(`Minumim bid:`, ethers.formatEther(minBid));
+      if (limit) {
+        const battleAmount = ethers.parseEther(limit);
+        if (minBid <= battleAmount) {
+          console.log(`Minimum bid is below bid limit`);
+          bid = minBid;
+        } else {
+          console.log(`Minimum bid is above bid limit`);
+        }
+      }
+      console.log("Bid amount:", ethers.formatEther(bid));
       if (bid < minBid) {
         console.log(
           `Bid is too low, minimum bid is ${ethers.formatEther(minBid)} MANA`
